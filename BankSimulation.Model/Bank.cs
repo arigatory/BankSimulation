e860cee80bc48bdb222708c38e1e6398cfbffb32
Ivan MSI <arigatory@gmail.com>
2021-05-26 23:05:20 +0300

    Add project files.

---
+++ BankSimulation.Model/Bank.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BankSimulation.Model
+{
+    public class Bank
+    {
+    }
+}
---
+++ BankSimulation.Model/Client.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BankSimulation.DataAccess
+{
+    public class Client
+    {
+    }
+}
